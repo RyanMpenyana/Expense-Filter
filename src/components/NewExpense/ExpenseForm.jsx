f087572a1f5363a532327a64abd19febf2dc16c2
admin <admin@MacHDs-MacBook-Pro.local>
2023-07-19 18:41:53 +0200

    remapping concept

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,39 +1,36 @@
 import { useState } from 'react';
 import './ExpenseForm.css';
 const ExpenseForm = () => {
-  const [userInput, setUserInput] = useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: ''
-  });
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
   const titleChangeHandler = event => {
-    setUserInput(() => {
-      return {
-        ...userInput,
-        enteredTitle: console.log(event.target.value)
-      };
-    });
+    setEnteredTitle(event.target.value);
   };
+
   const amountChangeHandler = event => {
-    setUserInput(() => {
-      return {
-        ...userInput,
-        enteredAmount: console.log(event.target.value)
-      };
-    });
+    setEnteredAmount(event.target.value);
   };
+
   const dateChangeHandler = event => {
-    setUserInput(() => {
-      return {
-        ...userInput,
-        enteredDate: console.log(event.target.value)
-      };
-    });
+    setEnteredDate(event.target.value);
   };
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    };
+    console.log(expenseData);
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label htmlFor="title">title</label>
@@ -59,7 +56,9 @@ const ExpenseForm = () => {
           </div>
         </div>
         <div className="new-expense__actions">
-          <button type="submit">Add Expense</button>
+          <button onClick={submitHandler} type="submit">
+            Add Expense
+          </button>
         </div>
       </form>
     </>
